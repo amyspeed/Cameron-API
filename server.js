a6e3a2b7de8259cbfc626557ba8d0365e034f90b
Amy Speed <amy.speed.henley@gmail.com>
2020-06-02 00:01:02 -0500

    tests, config, travis

--- server.js
+++ server.js
@@ -5,40 +5,35 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 
-const app = express();
+const { PORT, EMAIL_HOST, EMAIL_USER, EMAIL_PASS, EMAIL_TO } = require('./config');
 
-const port = process.env.PORT || 8080;
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-app.listen(port, () => {
-  console.log('We are live on port 8080');
-});
-
-
 app.get('/', (req, res) => {
-  res.send('Welcome to my api');
+    res.json({ok: true});
 });
 
 app.post('/api/v1', (req,res) => {
   var data = req.body;
 
   let transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
+    host: EMAIL_HOST,
     port: 465,
     secure: true,
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        user: EMAIL_USER,
+        pass: EMAIL_PASS
     }
 });
 
 var mailOptions = {
   from: data.email,
-  to: process.env.EMAIL_TO,
+  to: EMAIL_TO,
   subject: data.subject,
   html: `<p>${data.firstName} ${data.lastName}</p>
           <p>${data.email}</p>
@@ -50,16 +45,52 @@ transporter.sendMail(mailOptions,
         if (error) {
             console.log(data);
             console.log(mailOptions);
-            console.log("ERROR")
+            console.log('ERROR')
             console.log(error)
             res.send(error)
         }
         else {
             console.log(data);
             console.log(mailOptions);
-            console.log("SUCCESS")
+            console.log('SUCCESS')
             res.send('Success')
         }
         transporter.close();
     });
 });
+
+app.use('*', function (req, res) {
+    res.status(404).json({ message: `Not Found` });
+});
+
+let server;
+
+function runServer(port = PORT) {
+    return new Promise((resolve, reject) => {
+            server = app.listen(port, () => {
+                console.log(`App is listening on port ${port}`);
+                resolve();
+            })
+                .on('error', err => {
+                    reject(err);
+                });
+    });
+}
+
+function closeServer() {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+}
+
+if (require.main === module) {
+    runServer().catch(err => console.error(err));
+}
+
+module.exports = { runServer, app, closeServer };
